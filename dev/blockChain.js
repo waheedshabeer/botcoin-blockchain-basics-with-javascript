978de9bd03b45d5fe0dfa7a9eb0b19e76568000f
Waheed <waheed.shabeer@gmail.com>
2022-08-26 02:56:22 +0500

    Basic BlockChain Working

---
+++ dev/blockChain.js
@@ -0,0 +1,41 @@
+function BotCoin() {
+  this.chain = [];
+  this.pendingTransactions = [];
+}
+
+BotCoin.prototype.createNewBotCoin = function (nonce, previousHash, hash) {
+  const newBotCoin = {
+    index: this.chain.length,
+    nonce,
+    previousHash,
+    hash,
+    timeStamp: Date.now(),
+    pendingTransactions: this.pendingTransactions,
+  };
+
+  this.pendingTransactions = [];
+  this.chain.push(newBotCoin);
+  return newBotCoin;
+};
+
+BotCoin.prototype.getLastNode = function (params) {
+  return this.chain[this.chain.length - 1];
+};
+
+BotCoin.prototype.createNewTransaction = function (
+  amount,
+  senderAddress,
+  receiverAddress
+) {
+  const newTransaction = {
+    amount,
+    senderAddress,
+    receiverAddress,
+  };
+
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastNode["index"] + 1;
+};
+
+module.exports = BotCoin;
--- dev/runTest.js
+++ dev/runTest.js
@@ -1,3 +1,14 @@
 const { logData } = require("../utils/log");
+const BotCoin = require("./blockChain");
 
-logData("Hello BlockChain");
+const botCoin = new BotCoin();
+botCoin.createNewBotCoin(1, "1", "2");
+botCoin.createNewTransaction(100, "Waheed", "Asif");
+botCoin.createNewBotCoin(2, "2", "3");
+botCoin.createNewTransaction(200, "Waheed", "Shoaib");
+botCoin.createNewTransaction(100, "Waheed", "Shoaib");
+botCoin.createNewTransaction(2300, "Waheed", "Shoaib");
+botCoin.createNewTransaction(400, "Waheed", "Shoaib");
+botCoin.createNewBotCoin(3, "3", "4");
+
+logData(botCoin);
